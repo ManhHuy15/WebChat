e77b4b81c14a2a44a054c1047a73598bbf2b6dc0
ManhHuy15 <huya3k52@gmail.com>
2025-06-23 09:21:36 +0700

    scroll to bottom and loading spinner message

--- Services/AuthenServices/AuthService.cs
+++ Services/AuthenServices/AuthService.cs
@@ -22,8 +22,7 @@ namespace Services.AuthenServices
         private readonly IUserRepository _userRepository;
         private readonly IJWTService _jwtService;
         private readonly IPasswordHashingService _passwordHashingService;
-        private readonly string MALE_AVATAR_DEFAULT = "https://res.cloudinary.com/ddg2gdfee/image/upload/v1738900139/avatar_female_default_tkxgkl.jpg";
-        private readonly string FEMALE_AVATAR_DEFAULT = "https://res.cloudinary.com/ddg2gdfee/image/upload/v1738900139/avatar_female_default_tkxgkl.jpg";
+        private readonly string AVATAR_DEFAULT = "https://res.cloudinary.com/ddg2gdfee/image/upload/v1750643519/default_avata_dry3fp.png";
 
         public AuthService(IUserRepository userRepository, IJWTService jwtService, IPasswordHashingService passwordHashingService)
         {
@@ -98,7 +97,7 @@ namespace Services.AuthenServices
 
             User newUser = new User{
                 FullName = registerUserDTOs.FullName,
-                Avatar = registerUserDTOs.Gender? MALE_AVATAR_DEFAULT : FEMALE_AVATAR_DEFAULT,
+                Avatar = AVATAR_DEFAULT,
                 Email = registerUserDTOs.Email,
                 Birth = registerUserDTOs.Birth,
                 Gender = registerUserDTOs.Gender,
@@ -137,7 +136,7 @@ namespace Services.AuthenServices
                 {
                     FullName = name,
                     Email = email,
-                    Avatar = MALE_AVATAR_DEFAULT,
+                    Avatar = AVATAR_DEFAULT,
                     Password = _passwordHashingService.HashPassword(id),
                     Role = (int)Enums.Role.User,
                     GoogleId = id,
